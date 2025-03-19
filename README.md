ff6477d7e1db35b1dec50eabfcff72da553f39bc
Volodymyr Fufalko <devfinkord@gmail.com>
2025-03-19 18:46:09 +0200

    API method added

--- README.md
+++ README.md
@@ -1,31 +1,42 @@
-# Example Dashboard Application
+# REPORT Homework Flask
 
+Dockerfile optimized with changing Ubuntu for Python-Alpine image.
+Size shrinked to ~115MB.
+
+![Size of builded backend image](buildedImages.png)
+
+Also implemented API functions for router calls(dashboard.py file).
+
+# Example Dashboard Application
 
 This is an example of dashboard application that organizes useful links into groups. The backend is based on Flask and frontend is served by Nginx.
 
 ## Installation
 
 1. **Clone the repository:**
-    ```sh
-    git clone https://github.com/arkma/dashboard-headless.git
-    cd dashboard-headless
-    ```
+
+   ```sh
+   git clone https://github.com/arkma/dashboard-headless.git
+   cd dashboard-headless
+   ```
 
 2. **Generate initial database data:**
-    ```sh
-    flask --app backend/dashboard init-db
-    ```
+
+   ```sh
+   flask --app backend/dashboard init-db
+   ```
 
 3. **Build the containers:**
-    ```sh
-    docker-compose build
-    ```
+   ```sh
+   docker-compose build
+   ```
 4. **Start the app:**
-    ```sh
-    docker-compose up
-    ```
 
-3. **Open the app in your browser:**
-    ```
-    http://127.0.0.1:80
-    ```
+   ```sh
+   docker-compose up
+   ```
+
+5. **Open the app in your browser:**
+   ```
+   http://127.0.0.1:80
+   ```
--- backend/dashboard/dashboard.py
+++ backend/dashboard/dashboard.py
@@ -24,13 +24,7 @@ def get_links():
     # Return all links
     db = get_db()
     links = db.execute('SELECT * FROM links').fetchall()
-    return jsonify({
-        "message":"Links",
-        "data":[dict(links) for links in links]
-    })
     return jsonify([{'id': link['id'], 'name': link['name'], 'url': link['link'], 'logo': link['logo']} for link in links])
-
-
 
 @bp.route("/links/groupped", methods=['GET'])
 def get_links_groupped():
@@ -39,16 +33,37 @@ def get_links_groupped():
 
 @bp.route('/links', methods=['POST'])
 def add_link():
-    pass
-    ## TODO: Add a new link 
+    db = get_db()
+    data = request.form
+
+    if not data:
+        return jsonify({"error": "No data received"}), 400
+
+    db.execute('INSERT INTO links (name, link, logo, group_id) VALUES (?, ?, ?, ?)',
+               (data.get('link_name', ''), data.get('link_url', ''), data.get('link_logo', None), data.get('group', '')))
+    db.commit()
+
+    return jsonify({"message": "Link added successfully"})
+    # Add a new link 
 
 @bp.route('/groups', methods=['GET'])
 def get_groups():
-    pass
-    ## TODO: Return all groups
+    db = get_db()
+    groups = db.execute('SELECT * FROM groups').fetchall()
+    return jsonify({
+        "message": "Groups",
+        "groups": [dict(group) for group in groups]  
+    })
+    # Return all groups
 
 @bp.route('/groups', methods=['POST'])
 def add_group():
-    pass
-    ## TODO: Add a new group
+    db = get_db()
+    data = request.json
+    db.execute(
+    'INSERT INTO groups (group_name, icon) VALUES (?, ?)',
+    (data['new_group_name'], data['new_group_icon']))
+    db.commit()
+    return jsonify({"message": "Group added to database"})
+    # Add a new group
 
